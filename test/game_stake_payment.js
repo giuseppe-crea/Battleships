eb79eb4f271ca8066d310fa3a53e3814cad44bc1
Giuseppe Crea <g.crea2@studenti.unipi.it>
2023-05-20 05:29:57 +0200

    Finished writing unit tests for payment function.

--- test/game_stake_payment.js
+++ test/game_stake_payment.js
@@ -48,12 +48,37 @@ contract("Battleships", function (accounts) {
             var reverted = false;
             try{
                 // now we try to pay again and see if the EVM crashes
-                const reply = await battleships.payStake(1, {value: 100});
+                const reply = await battleships.payStake(1, {value: 5000});
             }
             catch(error){
                 reverted = true;
             }
             assert(reverted, "The transaction somehow went through!")
         });
+        it("Pay the wrong amount.", async () =>{
+            var reverted = false;
+            try{
+                // now we try to pay again and see if the EVM crashes
+                const reply = await battleships.payStake(1, {from: accounts[1], value: 100});
+            }
+            catch(error){
+                reverted = true;
+            }
+            assert(reverted, "The transaction somehow went through!")
+        });
+    });
+
+    describe("Finishing the stake payment procedure for game #1.", async () => {
+        it("Paying on second user.", async () => {
+            const reply = await battleships.payStake(1, {from: accounts[1], value: 5000});
+            assert.equal(reply.logs[0].event, 'StakePaid', "Event of type StakePaid did not fire.");
+            assert.equal(reply.logs[0].args[0], 1, "Event was emitted for the wrong gameID.");
+            assert.equal(reply.logs[0].args[1], accounts[1], "Event was emitted for the wrong from.");
+            assert.equal(reply.logs[1].event, 'AcceptingBoards', "Event of type AcceptingBoards did not fire.");
+        });
+        it("Making sure the game is in the correct state.", async () => {
+            const reply = await battleships.checkGameState(1);
+            assert.equal(reply, 3, "Game is not in PLACING_SHIPS state!")
+        })
     });
 });
